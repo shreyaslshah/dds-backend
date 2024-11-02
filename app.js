fd46a51c960745b3d9c57be196d89cdd2490b22a
Shreyas Shah <shreyaslshah@gmail.com>
2024-11-02 14:26:02 -0700

    c

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000; // You can change this port number if needed
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
